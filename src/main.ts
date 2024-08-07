18a2803c6c102e908e8cbb893520c5249e5db483
Guilherme Vieira <hi@guilhermevieira.dev>
2024-08-07 00:22:21 -0300

    feat(pokemon-module): create pokemon module and itself test

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from 'config/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, swaggerDocument);
+  app.enableCors({
+    origin: '*',
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/pokemons/pokemons.module.ts
+++ src/modules/pokemons/pokemons.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PokemonsService } from './pokemons.service';
 import { PokemonsController } from './pokemons.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [PokemonsController],
   providers: [PokemonsService],
 })
