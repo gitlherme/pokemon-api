c51730d3307dd75a527c8a99512ec6a58230edc3
Guilherme Vieira <hi@guilhermevieira.dev>
2024-08-07 00:22:02 -0300

    feat(pokemon-module): create pokemon controller and itself test

--- src/modules/pokemons/pokemons.controller.spec.ts
+++ src/modules/pokemons/pokemons.controller.spec.ts
@@ -1,20 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { PokemonDataMapper } from './dto/pokemons.dto';
+import { pokemonMock } from '../../../test/pokemons.mock';
 
 describe('PokemonsController', () => {
-  let controller: PokemonsController;
+  let iot: PokemonsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, HttpModule],
       controllers: [PokemonsController],
-      providers: [PokemonsService],
+      providers: [
+        PokemonsService,
+        {
+          provide: ConfigModule,
+          useValue: { get: jest.fn().mockReturnValue('https://example.com') },
+        },
+      ],
     }).compile();
 
-    controller = module.get<PokemonsController>(PokemonsController);
+    iot = module.get<PokemonsController>(PokemonsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call service with data transformation', async () => {
+    jest
+      .spyOn(PokemonsService.prototype, 'findOne')
+      .mockResolvedValueOnce(PokemonDataMapper.output(pokemonMock));
+    await iot.findOne({ name: 'Pikachu' });
+    expect(PokemonsService.prototype.findOne).toHaveBeenCalledWith(
+      'Pikachu'.toLowerCase(),
+    );
   });
 });
--- src/modules/pokemons/pokemons.controller.ts
+++ src/modules/pokemons/pokemons.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PokemonsService } from './pokemons.service';
+import { IsDefined, Matches } from 'class-validator';
+
+export class PokemonDTO {
+  @Matches(/^[A-Za-z]+$/, { message: 'The Pokémon name needs to be a string' })
+  @IsDefined({ message: 'The Pokémon needs a name' })
+  name: string;
+}
 
 @Controller('pokemons')
 export class PokemonsController {
   constructor(private readonly pokemonsService: PokemonsService) {}
 
   @Get(':name')
-  findOne(@Param('name') name: string) {
-    return this.pokemonsService.findOne(name);
+  findOne(@Param() pokemonDTO: PokemonDTO) {
+    return this.pokemonsService.findOne(pokemonDTO.name.toLowerCase());
   }
 }
